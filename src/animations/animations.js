3b932dec639ff0f170460883fe422fb01608e267
hassantayyab <hassandogar9@gmail.com>
2021-08-11 00:39:30 +0200

    Started animations

---
+++ src/animations/animations.js
@@ -0,0 +1,11 @@
+const slideUp = {
+  initial: { opacity: 0, scale: 0.5 },
+  animate: { scale: 1, opacity: 1 },
+}
+
+const scale = {
+  initial: { scale: 0.5, opacity: 0 },
+  animate: { scale: 1, opacity: 1 },
+}
+
+export { slideUp, scale }
---
+++ src/animations/transitions.js
@@ -0,0 +1,4 @@
+const primaryTransition = { transition: { duration: 0.75, ease: 'easeInOut' } }
+const secondaryTransition = { transition: { duration: 1, ease: 'easeInOut' } }
+
+export { primaryTransition, secondaryTransition }
