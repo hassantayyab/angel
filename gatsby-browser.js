7e3dfc451801ab486489fad8f0264d8e0e21c2e9
hassantayyab <hassandogar9@gmail.com>
2021-08-11 01:50:01 +0200

    Finally fixed

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,7 @@
 import './src/styles/global.css'
 
 export const onClientEntry = async () => {
-  if (typeof IntersectionObserver === `undefined`) {
-    await import(`intersection-observer`)
+  if (typeof IntersectionObserver === 'undefined') {
+    await import('intersection-observer')
   }
 }
